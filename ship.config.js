68e98382625ebcb237328ca0b82edf2381419175
Daichi Kimura <dkimura@bdd.xyz>
2019-12-23 08:32:09 +0900

    chore: Remove default setting

--- ship.config.js
+++ ship.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   publishCommand: ({ defaultCommand }) => `${defaultCommand} --access public`,
-  mergeStrategy: { toSameBranch: ['master'] },
   monorepo: {
     mainVersionFile: 'package.json',
     packagesToBump: '["packages/*"]',
