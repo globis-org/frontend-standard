0ec3449efe876e66789d8b415f41a3fdc03c3367
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2019-12-24 14:53:44 +0900

    chore: add minimum required import rules

--- packages/eslint-config-globis/index.js
+++ packages/eslint-config-globis/index.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ['airbnb', 'airbnb/hooks', './shared.js'],
+  rules: {
+    'import/order': ['error', { 'newlines-between': 'always' }],
+    'import/prefer-default-export': 0,
+  },
 }
