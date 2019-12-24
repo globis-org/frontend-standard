17a0963397448edb85df40fa2159ce9c44b1f083
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2019-12-24 14:57:38 +0900

    chore: add minimum required react rules

--- packages/eslint-config-globis/index.js
+++ packages/eslint-config-globis/index.js
@@ -3,5 +3,11 @@ module.exports = {
   rules: {
     'import/order': ['error', { 'newlines-between': 'always' }],
     'import/prefer-default-export': 0,
+    // FixMe https://github.com/yannickcr/eslint-plugin-react/issues/1846
+    'react/button-has-type': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'react/jsx-key': ['error', { checkFragmentShorthand: true }],
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0,
   },
 }
