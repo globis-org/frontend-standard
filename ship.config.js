8053cdfabba2f5e796f9992726ef7b149797f0a5
Daichi Kimura <dkimura@bdd.xyz>
2019-12-26 15:15:44 +0900

    chore: Remove pullRequestReviewer (#22)

--- ship.config.js
+++ ship.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   publishCommand: ({ defaultCommand }) => `${defaultCommand} --access public`,
-  pullRequestReviewer: ['globis-org/jser'],
   monorepo: {
     mainVersionFile: 'package.json',
     packagesToBump: ['packages/*'],
