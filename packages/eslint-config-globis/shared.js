5d244e8f972f39bde8ea95de8774a203dfa979ee
Daichi Kimura <dkimura@bdd.xyz>
2020-01-27 11:42:30 +0900

    feat: Add import/extensions rule (#60)

--- packages/eslint-config-globis/shared.js
+++ packages/eslint-config-globis/shared.js
@@ -9,6 +9,18 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['prettier', '@typescript-eslint'],
   rules: {
+    // FixMe https://github.com/benmosher/eslint-plugin-import/issues/1615
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        mjs: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
     '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
