b875e79ab02f7c2c01f29bb8bcfbaafaf4c67b1e
Daichi Kimura <dkimura@bdd.xyz>
2020-05-25 16:32:08 +0900

    chore: Remove plugin:@typescript-eslint/eslint-recommended rules

--- packages/eslint-config-globis/shared.js
+++ packages/eslint-config-globis/shared.js
@@ -1,6 +1,5 @@
 module.exports = {
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
