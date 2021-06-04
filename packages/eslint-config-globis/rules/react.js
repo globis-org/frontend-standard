c339bbdf44f9de13edbe3bf16d190a36187cdbd1
Ryoya HIRAYAMA <ryoya.hirayama@globis.co.jp>
2021-06-04 22:13:48 +0900

    fix: Turn off require-default-props (#236)
    Co-authored-by: Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>

--- packages/eslint-config-globis/rules/react.js
+++ packages/eslint-config-globis/rules/react.js
@@ -5,6 +5,7 @@ const rules = {
   'react/jsx-key': ['error', { checkFragmentShorthand: true }],
   'react/jsx-props-no-spreading': 'off',
   'react/prop-types': 'off',
+  'react/require-default-props': 'off',
 }
 
 module.exports = { rules }
